f79acfd59a31402c78dba43d9ad64d71b2db2f7a
Nitesh2-0niteshkumar61725@gmail.com <niteshkumar61725@gmail.com>
2024-05-27 17:50:54 +0530

    axios-Request

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,19 @@
+import axios from 'axios';
 import React from 'react'
 
 const About = () => {
+  async function aboutMe() {
+    try {
+      const {data} = await axios.get('/posts')
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
   return (
     <div className='text-2xl'>
       About
+      <button onClick={aboutMe}>Get Post</button>
     </div>
   )
 }
--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,27 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from '../helper/axios'
 import { NavLink, Outlet, useLocation, useNavigate } from 'react-router-dom';
 
 const User = () => {
   const navigate = useNavigate();
   const { pathname } = useLocation()
-  const submitHandler = (e) => {
-    e.preventDefault();
-    console.log('Form is Submited');
-    navigate('/')
-  }
+  const [user,setUser] = useState(null)
+
+  const getUser = async () => {
+    try {
+      const {data} =  await axios.get('/users')
+      setUser(data)
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  } 
+
+  // const submitHandler = (e) => {
+  //   e.preventDefault();
+  //   console.log('Form is Submited');
+  //   navigate('/')
+  // }
   return (
     <div>
       <h1>Hello World, This is the user</h1>
-      <form onSubmit={submitHandler}>
-        <button type='submit' className='bg-indigo-500 rounded-md p-2 ml-7 text-gray-100'>Submit Form</button>
-      </form>
+      {/* <form onSubmit={submitHandler}> type='submit' */}
+        <button onClick={getUser} className='bg-indigo-500 rounded-md p-2 ml-7 text-gray-100'>Get User</button>
+      {/* </form> */}
       <ul className='list-disc'>
-        <li className='list-item'> <NavLink className='list-item' to={`${pathname}/1`} >Iteam1</NavLink></li>
+        {user ? user.map((u) => {
+          <li className='list-item' key={u.id}>{u.name}</li>
+        }):'Loading.....'}
+        {/* <li className='list-item'> <NavLink className='list-item' to={`${pathname}/1`} >Iteam1</NavLink></li>
         <li className='list-item'> <NavLink className='list-item' to={`${pathname}/2`} >Iteam1</NavLink></li>
-        <li className='list-item'> <NavLink className='list-item' to={`${pathname}/3`} >Iteam1</NavLink></li>
+        <li className='list-item'> <NavLink className='list-item' to={`${pathname}/3`} >Iteam1</NavLink></li> */}
       </ul >
-      <hr />
-      <Outlet />
+      {/* <hr /> */}
+      {/* <Outlet /> */}
     </div>
   )
 }
